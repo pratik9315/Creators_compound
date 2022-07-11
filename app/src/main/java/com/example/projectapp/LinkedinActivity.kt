4763a6f9e6f5ddb77bb2fd61061d2e9315ba94a1
pratik2315 <pratikhaldankar360@gmail.com>
2022-07-11 22:21:56 +0530

    minor changes

---
+++ app/src/main/java/com/example/projectapp/LinkedinActivity.kt
@@ -0,0 +1,12 @@
+package com.example.projectapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.projectapp.R
+
+class LinkedinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linkedin)
+    }
+}
